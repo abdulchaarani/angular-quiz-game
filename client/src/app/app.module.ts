8281e066b87636b8a5fd6b7dd60e245f378f805d
Ikramarroud <ikramarroud35@gmail.com>
2024-01-24 05:15:40 +0000

    updated appmodule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
@@ -11,6 +12,20 @@ import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { CreateQuestionComponent } from '@app/pages/create-question/create-question.component';
+import { AdminQuestionBankComponent } from './pages/admin-question-bank/admin-question-bank.component';
+import { QuestionListItemComponent } from './components/question-list-item/question-list-item.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { SortByLastModificationPipe } from './sort-by-last-modification.pipe';
+import { QuestionComponent } from './components/question/question.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+
 
 /**
  * Main module that is used in main.ts.
@@ -19,8 +34,36 @@ import { MaterialPageComponent } from '@app/pages/material-page/material-page.co
  * Otherwise Angular Cli will not know in which module to put new component
  */
 @NgModule({
-    declarations: [AppComponent, GamePageComponent, MainPageComponent, MaterialPageComponent, PlayAreaComponent, SidebarComponent],
-    imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule],
+    declarations: [
+        AppComponent,
+        GamePageComponent,
+        MainPageComponent,
+        MaterialPageComponent,
+        PlayAreaComponent,
+        SidebarComponent,
+        AdminQuestionBankComponent,
+        QuestionListItemComponent,
+        SortByLastModificationPipe,
+        QuestionComponent,
+        CreateQuestionComponent,
+    ],
+    imports: [
+        AppMaterialModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        DragDropModule,
+        MatSliderModule,
+        MatCheckboxModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatCardModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })

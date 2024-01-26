efaf5aa6a22ccb9cf27b6eb3a9a09f0ad63cf26a
nadabfh <112907983+nadabfh@users.noreply.github.com>
2024-01-26 09:33:37 -0500

    view of question-area-component

--- client/src/app/components/question-area/question-area.component.ts
+++ client/src/app/components/question-area/question-area.component.ts
@@ -2,23 +2,24 @@ import { Component } from '@angular/core';
 // import { TimeService } from '@app/services/time.service';
 // import { Question } from '@app/interfaces/question';
 import { Choice } from '@app/interfaces/choice';
+import { Game } from '@app/interfaces/game';
 
 @Component({
-  selector: 'app-question-area',
-  templateUrl: './question-area.component.html',
-  styleUrls: ['./question-area.component.scss']
+    selector: 'app-question-area',
+    templateUrl: './question-area.component.html',
+    styleUrls: ['./question-area.component.scss'],
 })
 export class QuestionAreaComponent {
-  // constructor( private timer : TimeService){}
+    // constructor( private timer : TimeService){}
 
-  currentQuestionIndex : number;
-  playerScore          : number;
-  questions            : Choice[];
-  // TODO : Timer
+    currentGame: Game;
+    currentQuestionIndex: number;
+    playerScore: number;
+    answers: Choice[];
 
-  submit() : void {}
-  checkAnswers() : void {}
-  abandon() : void {}
+    // TODO : Timer
 
-
+    submit(): void {}
+    checkAnswers(): void {}
+    abandon(): void {}
 }

9eaf9d47d020a86a0046229121bdb4ec39b5f83f
Ikramarroud <ikramarroud35@gmail.com>
2024-01-26 22:08:47 +0000

    fixed lint

--- client/src/app/pages/create-question/create-question.component.spec.ts
+++ client/src/app/pages/create-question/create-question.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreateQuestionComponent } from './create-question.component';
 
 describe('CreateQuestionComponent', () => {
-  let component: CreateQuestionComponent;
-  let fixture: ComponentFixture<CreateQuestionComponent>;
+    let component: CreateQuestionComponent;
+    let fixture: ComponentFixture<CreateQuestionComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CreateQuestionComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [CreateQuestionComponent],
+        });
+        fixture = TestBed.createComponent(CreateQuestionComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(CreateQuestionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

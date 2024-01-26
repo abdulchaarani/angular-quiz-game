be4bbcf266c7fec13bdd3eee513b8a04c42d9cfa
Adam Kassi Lahlou <kassilahlouadam@gmail.com>
2024-01-26 15:15:27 -0500

    Fix lint

--- client/src/app/pages/admin-page/admin-questions-list/admin-questions-list.component.spec.ts
+++ client/src/app/pages/admin-page/admin-questions-list/admin-questions-list.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdminQuestionsListComponent } from './admin-questions-list.component';
 
 describe('AdminQuestionsListComponent', () => {
-  let component: AdminQuestionsListComponent;
-  let fixture: ComponentFixture<AdminQuestionsListComponent>;
+    let component: AdminQuestionsListComponent;
+    let fixture: ComponentFixture<AdminQuestionsListComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [AdminQuestionsListComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [AdminQuestionsListComponent],
+        });
+        fixture = TestBed.createComponent(AdminQuestionsListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(AdminQuestionsListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

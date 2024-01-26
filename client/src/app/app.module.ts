cb9a15e1f164232b646f53dcbe5d1c15048ef806
Adam Kassi Lahlou <kassilahlouadam@gmail.com>
2024-01-26 09:50:27 -0500

    Fix import modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,15 +7,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { AppComponent } from '@app/pages/app/app.component';
+import { PlayAreaComponent } from './components/play-area/play-area.component';
+import { QuestionListItemComponent } from './components/question-list-item/question-list-item.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { AdminQuestionBankComponent } from './pages/admin-page/admin-question-bank/admin-question-bank.component';
+import { AdminQuestionsListComponent } from './pages/admin-page/admin-questions-list/admin-questions-list.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HostPageComponent } from './pages/host-page/host-page.component';
 import { PlayerPageComponent } from './pages/player-page/player-page.component';
-import { QuestionListItemComponent } from './components/question-list-item/question-list-item.component';
-import { AdminQuestionBankComponent } from './pages/admin-page/admin-question-bank/admin-question-bank.component';
 import { SortByLastModificationPipe } from './sort-by-last-modification.pipe';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { PlayAreaComponent } from './components/play-area/play-area.component';
 /**
  * Main module that is used in main.ts.
  * All automatically generated components will appear in this module.
@@ -33,6 +34,7 @@ import { PlayAreaComponent } from './components/play-area/play-area.component';
         HostPageComponent,
         QuestionListItemComponent,
         AdminQuestionBankComponent,
+        AdminQuestionsListComponent,
         SortByLastModificationPipe,
     ],
     imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule, DragDropModule],

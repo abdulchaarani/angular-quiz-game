c828ee7a7799349f45409eaf959ff3b09867db7e
Ikramarroud <ikramarroud35@gmail.com>
2024-01-26 21:00:16 +0000

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
@@ -13,19 +12,15 @@ import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { CreateQuestionComponent } from '@app/pages/create-question/create-question.component';
-import { AdminQuestionBankComponent } from './pages/admin-question-bank/admin-question-bank.component';
-import { QuestionListItemComponent } from './components/question-list-item/question-list-item.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { SortByLastModificationPipe } from './sort-by-last-modification.pipe';
-import { QuestionComponent } from './components/question/question.component';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 /**
  * Main module that is used in main.ts.
@@ -41,10 +36,6 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
         MaterialPageComponent,
         PlayAreaComponent,
         SidebarComponent,
-        AdminQuestionBankComponent,
-        QuestionListItemComponent,
-        SortByLastModificationPipe,
-        QuestionComponent,
         CreateQuestionComponent,
     ],
     imports: [

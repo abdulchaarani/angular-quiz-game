fd31ddd8507cb64dc7347953de4235e3e7fcbd6d
Hib00boo <chaaranihiba@outlook.com>
2024-02-02 17:03:26 -0500

    Fixed issues with detecting deleted games and refreshing page

--- client/src/app/pages/host-page/host-page.component.ts
+++ client/src/app/pages/host-page/host-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Game } from '@app/interfaces/game';
 import { GamesService } from '@app/services/games.service';
+import { NotificationService } from '@app/services/notification.service';
 
 @Component({
     selector: 'app-host-page',
@@ -11,16 +12,19 @@ import { GamesService } from '@app/services/games.service';
 export class HostPageComponent implements OnInit {
     games: Game[] = [];
     selectedGame: Game;
+    gameIsValid: boolean;
 
     constructor(
         private gameService: GamesService,
+        private notificationService: NotificationService,
         public snackBar: MatSnackBar,
-    ) {}
+    ) {
+        this.gameIsValid = false;
+    }
 
     ngOnInit(): void {
         this.gameService.getGames().subscribe((data: Game[]) => {
             this.games = data.filter((game) => game.isVisible);
-            // this.games = [...data];
         });
     }
 
@@ -30,16 +34,27 @@ export class HostPageComponent implements OnInit {
         });
     }
 
-    selectGame(selectedGame?: Game): void {
-        try {
-            if (!selectedGame) {
-                throw new Error("Le jeu sélectionné n'existe plus");
-            }
-            if (selectedGame.isVisible === false) {
-                throw new Error("Le jeu sélectionné n'est plus visible");
-            }
+    loadSelectedGame(selectedGame: Game): void {
+        this.gameService.getGameById(selectedGame.id).subscribe({
+            next: (data: Game) => (this.selectedGame = data),
+            error: () => {
+                const snackBarRef = this.notificationService.displayErrorMessageAction("Le jeu sélectionné n'existe plus", 'Actualiser');
+                // const snackBarRef = this.snackBar.open("Le jeu sélectionné n'existe plus", 'Actualiser');
+                snackBarRef.onAction().subscribe(() => {
+                    this.loadGames();
+                });
+            },
+        });
+    }
 
-            this.selectedGame = selectedGame;
+    selectGame(selectedGame: Game): void {
+        this.loadSelectedGame(selectedGame);
+        try {
+            if (this.selectedGame.isVisible === false) {
+                throw new Error("Le jeu sélectionné n'est plus visible");
+            } else {
+                this.gameIsValid = true;
+            }
         } catch (error) {
             let errorMessage;
             if (error instanceof Error) {

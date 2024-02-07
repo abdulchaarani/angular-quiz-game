cb156c12f004d624ba4a45999ddbe50101d59188
Ikramarroud <ikramarroud35@gmail.com>
2024-02-07 03:27:34 +0000

    added addQuestion Method

--- client/src/app/services/games.service.ts
+++ client/src/app/services/games.service.ts
@@ -1,38 +1,41 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Game } from '@app/interfaces/game';
-import { Observable } from 'rxjs';
+import { Observable} from 'rxjs';
 import { ApiService } from './api.service';
+import { Question } from '@app/interfaces/question';
 
 @Injectable({
     providedIn: 'root',
 })
 export class GamesService extends ApiService<Game> {
-    constructor(http: HttpClient) {
+    constructor(http: HttpClient){
+       // private gamesApiService : ApiService<Game>, 
+        //private questionApiService: ApiService<Question>, ) {
         super(http, 'admin/games');
     }
 
     getGames(): Observable<Game[]> {
         return this.getAll();
     }
-    getGameById(id: number): Observable<Game> {
-        return this.getById(id.toString());
+    getGameById(id: string): Observable<Game> {
+        return this.getById('', id);
     }
 
     toggleGameVisibility(game: Game): Observable<HttpResponse<string>> {
         game.isVisible = !game.isVisible;
-        return this.update(game, game.id.toString());
+        return this.update(game.id);
     }
 
-    deleteGame(id: number): Observable<HttpResponse<string>> {
-        return this.delete(id.toString());
+    deleteGame(id: string): Observable<HttpResponse<string>> {
+        return this.delete(id);
     }
 
     uploadGame(newGame: Game) {
         return this.add(newGame, '');
     }
 
-    // Keep it in Front-end for now
+
     downloadGameAsJson(gameToStringify: Game): void {
         const stringifiedGame = JSON.stringify(gameToStringify, (key, value) => {
             if (key !== 'isVisible' && key !== '_id' && key !== '__v') {
@@ -49,4 +52,9 @@ export class GamesService extends ApiService<Game> {
         window.URL.revokeObjectURL(url);
         downloadLink.remove();
     }
-}
+
+    addQuestionToGame(gameId: string, addedQuestion: Question): Observable<HttpResponse<string>> {
+        return this.addQuestion(addedQuestion, gameId);
+    }
+    }
+

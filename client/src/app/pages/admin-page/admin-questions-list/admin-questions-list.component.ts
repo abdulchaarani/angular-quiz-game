12869f28b9a991e510b8ce37bc08b0099e8a0bf4
Ikramarroud <ikramarroud35@gmail.com>
2024-02-08 00:23:04 +0000

    added submit/pop-up screen

--- client/src/app/pages/admin-page/admin-questions-list/admin-questions-list.component.ts
+++ client/src/app/pages/admin-page/admin-questions-list/admin-questions-list.component.ts
@@ -1,12 +1,14 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , EventEmitter, Output} from '@angular/core';
 import { Game } from '@app/interfaces/game';
 import { Question } from '@app/interfaces/question';
+import { CreateQuestionComponent } from '@app/pages/create-question/create-question.component';
 // import { GamesCreationService } from '@app/services/games-creation.service';
 import { GamesService } from '@app/services/games.service';
 // import { QuestionService } from '@app/services/question.service';
 import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
     selector: 'app-admin-questions-list',
@@ -14,16 +16,27 @@ import { ActivatedRoute } from '@angular/router';
     styleUrls: ['./admin-questions-list.component.scss'],
 })
 export class AdminQuestionsListComponent implements OnInit {
+    dialogState: boolean = false;
+
+    @Output() createQuestionEvent: EventEmitter<Question> = new EventEmitter<Question>();
+    game: Game ;
     response: string = '';
 
     constructor(
-        // private readonly questionService: QuestionService,
-        // private readonly gamesCreationService: GamesCreationService,
+        public dialog: MatDialog,
         private readonly gamesService: GamesService,
         private route: ActivatedRoute,
-    ) {}
+    ) 
 
-    game: Game;
+    {}
+    drop(event: CdkDragDrop<Question[]>) {
+        moveItemInArray(this.game.questions, event.previousIndex, event.currentIndex);
+    }
+
+    changeDuration(event: Event) {
+        this.game.duration = Number((event.target as HTMLInputElement).value);
+    }
+
 
     isValid: boolean = false;
 
@@ -37,14 +50,6 @@ export class AdminQuestionsListComponent implements OnInit {
         });
     }
 
-    drop(event: CdkDragDrop<Question[]>) {
-        moveItemInArray(this.game.questions, event.previousIndex, event.currentIndex);
-    }
-
-    changeDuration(event: Event) {
-        this.game.duration = Number((event.target as HTMLInputElement).value);
-    }
-
     deleteQuestion(questionId: string) {
         if (this.game.questions.length === 1 || this.game.id === null) {
             return;
@@ -52,19 +57,8 @@ export class AdminQuestionsListComponent implements OnInit {
         this.game.questions = this.game.questions.filter((question: Question) => question.id !== questionId);
     }
 
-    addNewGame() {
-        const newQuestion: Question = {
-            id: '',
-            type: 'QCM',
-            text: 'Quelle est la question?',
-            points: 20,
-            lastModification: '2024-01-26T14:21:19+00:',
-        };
-
-        this.game.questions.push(newQuestion);
-    }
-
     saveGame() {
+        console.log(this.game.questions);
         this.gamesService.replaceGame(this.game).subscribe((response: HttpResponse<string>) => {
             () => {
                 this.response = 'Game saved';
@@ -73,5 +67,34 @@ export class AdminQuestionsListComponent implements OnInit {
                 this.response = 'Game not saved';
             };
         });
+    }
+
+    addNewQuestion(newQuestion: Question) {
+        console.log("new", newQuestion);
+        this.game.questions.push(newQuestion);
+    }
+
+    toggleCreateQuestion() {
+        this.dialogState = !this.dialogState;
+    }
+
+    dialogRef: any;
+
+    // https://stackoverflow.com/questions/47592364/usage-of-mat-dialog-close
+    openDialog() {
+        if (!this.dialogState) {
+            this.dialogRef = this.dialog.open(CreateQuestionComponent, {
+                height: '70%',
+                width: '100%',
+            });
+            this.dialogRef.componentInstance.createQuestionEvent.subscribe((newQuestion: Question) => {
+                if (newQuestion) {
+                    this.addNewQuestion(newQuestion);
+                    this.dialogRef.close();
+                }
+
+                this.dialogState = false;
+            });
+        }
     }
 }

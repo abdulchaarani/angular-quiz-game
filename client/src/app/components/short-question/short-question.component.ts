c3cdd278a66153eb1e45ecbd0ca5dec75c7deb2d
Your Name <abdul@chaarani.ca>
2024-02-09 09:14:50 -0500

    Removed question point coloring

--- client/src/app/components/short-question/short-question.component.ts
+++ client/src/app/components/short-question/short-question.component.ts
@@ -1,12 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Question } from '@app/interfaces/question';
 
-enum PointRange {
-    Low = 10,
-    Medium = 30,
-    High = 50,
-}
-
 @Component({
     selector: 'app-short-question',
     templateUrl: './short-question.component.html',
@@ -14,14 +8,4 @@ enum PointRange {
 })
 export class ShortQuestionComponent {
     @Input() question: Question;
-
-    getPointsColor(): string {
-        if (this.question.points >= PointRange.High) {
-            return 'high-points';
-        } else if (this.question.points >= PointRange.Medium) {
-            return 'medium-points';
-        } else {
-            return 'low-points';
-        }
-    }
 }

7625dbe154289e87b65f44e43e954111b9a5bae5
Verocayden <victoca@bell.net>
2024-02-08 20:10:33 -0500

    Started Implementation of Admin Authentication Client Side

--- client/src/app/services/authentication/authentication.service.ts
+++ client/src/app/services/authentication/authentication.service.ts
@@ -1,23 +1,52 @@
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { NotificationService } from '../notification.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AuthenticationService {
-    constructor(private readonly http: HttpClient) {}
+    private isAuthenticated: boolean;
+    constructor(
+        private readonly http: HttpClient,
+        private router: Router,
+        private readonly notificationService: NotificationService,
+    ) {
+        this.isAuthenticated = false;
+    }
 
-    validatePassword(inputUsername: string, inputPassword: string): Observable<HttpResponse<Object>> {
+    validatePassword(inputUsername: string, inputPassword: string): void {
         const contentJsonHeader = new HttpHeaders({
             'Content-Type': 'application/json',
         });
-        return this.http
+
+        this.http
             .post(`${environment.serverUrl}/login`, JSON.stringify({ username: inputUsername, password: inputPassword }), {
                 observe: 'response',
                 headers: contentJsonHeader,
             })
-            .pipe(catchError((error: Error) => throwError(() => new Error('Mauvais mot de passe'))));
+            .subscribe({
+                next: () => {
+                    this.router.navigate(['/admin/games']);
+                    this.isAuthenticated = true;
+                },
+                error: () => this.notificationService.displayErrorMessage(`Le mot de passe est invalide.`),
+            });
+    }
+
+    getIsAuthenticated() {
+        return this.isAuthenticated;
     }
 }
+
+export const AuthGuard = () => {
+    const auth = inject(AuthenticationService);
+    const router = inject(Router);
+    if (!auth.getIsAuthenticated()) {
+        router.navigateByUrl('/home');
+        return false;
+    }
+    return true;
+};

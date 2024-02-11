a34e30d87f386847a768d556b91df8c86b3222c8
Verocayden <victoca@bell.net>
2024-02-11 10:35:48 -0500

    Removed unused code

--- server/app/services/example/example.service.spec.ts
+++ server/app/services/example/example.service.spec.ts
@@ -1,5 +1,3 @@
-import { Message } from '@app/model/schema/message.schema';
-import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExampleService } from './example.service';
 
@@ -8,42 +6,13 @@ describe('ExampleService', () => {
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [ExampleService, Logger],
+            providers: [ExampleService],
         }).compile();
 
         service = module.get<ExampleService>(ExampleService);
     });
 
     it('should be defined', () => {
-        expect(service).toBeDefined();
-    });
-
-    it('about() should return the about message', () => {
-        expect(service.about().title.toLowerCase()).toContain('about');
-    });
-
-    it('helloWorld() should return the hello world message', () => {
-        expect(service.helloWorld().title.toLowerCase()).toContain('hello world');
-    });
-
-    it('storeMessage() should save message', () => {
-        const allMessages = service.getAllMessages();
-        expect(allMessages.length).toEqual(0);
-        const dummyMessage1: Message = {
-            title: 'Hey',
-            body: 'you',
-        };
-        service.storeMessage(dummyMessage1);
-        const allMessagesAfterFirstStore = service.getAllMessages();
-        expect(allMessagesAfterFirstStore.length).toEqual(1);
-        const dummyMessage2: Message = {
-            title: 'Hey',
-            body: 'there',
-        };
-        service.storeMessage(dummyMessage2);
-        const allMessagesAfterSecondSave = service.getAllMessages();
-        expect(allMessagesAfterSecondSave.length).toEqual(2);
-        expect(allMessages.sort()).toEqual([dummyMessage1, dummyMessage2].sort());
         expect(service).toBeDefined();
     });
 });

6b7b4bd8b0dcf18ae428c6a50c9cbf67d489c3ff
Ikramarroud <ikramarroud35@gmail.com>
2024-02-11 17:11:43 +0000

    added tests for submitting question to game and/or bank of questions

--- client/src/app/pages/admin-page/admin-questions-list/admin-questions-list.component.spec.ts
+++ client/src/app/pages/admin-page/admin-questions-list/admin-questions-list.component.spec.ts
@@ -1,69 +1,93 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { AdminQuestionsListComponent } from './admin-questions-list.component';
-// import { GamesService } from '@app/services/games.service';
-// import { HttpClientModule } from '@angular/common/http';
-// import { ActivatedRoute } from '@angular/router';
-// import { of } from 'rxjs';
-// import { Game } from '@app/interfaces/game';
-// import { MatDialog } from '@angular/material/dialog';
-// import { MatSnackBar } from '@angular/material/snack-bar';
-// import { SortByLastModificationPipe } from '@app/pipes/sort-by-last-modification.pipe';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AdminQuestionsListComponent } from './admin-questions-list.component';
+import { GamesService } from '@app/services/games.service';
+import { HttpClientModule, HttpResponse } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { Game } from '@app/interfaces/game';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SortByLastModificationPipe } from '@app/pipes/sort-by-last-modification.pipe';
+import { Question } from '@app/interfaces/question';
+import { QuestionService } from '@app/services/question.service';
+import { NotificationService } from '@app/services/notification.service';
 
 describe('AdminQuestionsListComponent', () => {
-    // let component: AdminQuestionsListComponent;
-    // let fixture: ComponentFixture<AdminQuestionsListComponent>;
-    // let gamesServiceSpy: jasmine.SpyObj<GamesService>;
-    // const mockGame: Game = {
-    //     id: '1',
-    //     description: 'Test game',
-    //     title: 'Test game',
-    //     duration: 10,
-    //     lastModification: '2018-11-13T20:20:39+00:00',
-    //     questions: [
-    //         {
-    //             id: '1',
-    //             type: 'QCM',
-    //             text: 'Combien de motifs blancs et noirs y a-t-il respectivement sur un ballon de soccer?',
-    //             points: 20,
-    //             lastModification: '2018-11-13T20:20:39+00:00',
-    //         },
-    //         {
-    //             id: '2',
-    //             type: 'QCM',
-    //             text: "Le ratio d'or est de 1:1.618, mais connaissez-vous le ratio d'argent?",
-    //             points: 40,
-    //             lastModification: '2024-01-20T14:17:39+00:00',
-    //         },
-    //     ],
-    // };
-    // beforeEach(() => {
-    //     const route = {
-    //         params: of({ id: '1' }),
-    //     };
-    //     gamesServiceSpy = jasmine.createSpyObj('GamesService', [
-    //         'getGames',
-    //         'getGameById',
-    //         'toggleGameVisibility',
-    //         'deleteGame',
-    //         'uploadGame',
-    //         'downloadGameAsJson',
-    //         'replaceGame',
-    //     ]);
-    //     TestBed.configureTestingModule({
-    //         imports: [HttpClientModule],
-    //         declarations: [AdminQuestionsListComponent, SortByLastModificationPipe],
-    //         providers: [
-    //             { provide: GamesService, useValue: gamesServiceSpy },
-    //             { provide: ActivatedRoute, useValue: route },
-    //             { provide: MatDialog, useValue: {} },
-    //             { provide: MatSnackBar, useValue: {} },
-    //         ],
-    //     });
-    //     fixture = TestBed.createComponent(AdminQuestionsListComponent);
-    //     component = fixture.componentInstance;
-    //     fixture.detectChanges();
-    //     component.game = mockGame;
-    // });
+    let component: AdminQuestionsListComponent;
+    let fixture: ComponentFixture<AdminQuestionsListComponent>;
+    let gamesServiceSpy: jasmine.SpyObj<GamesService>;
+    let questionServiceSpy: jasmine.SpyObj<QuestionService>;
+    let notificationServiceSpy: jasmine.SpyObj<NotificationService>;
+    const mockHttpResponse: HttpResponse<string> = new HttpResponse({ status: 200, statusText: 'OK' });
+    let matDialogSpy: jasmine.SpyObj<MatDialog>;
+
+    const mockGame: Game = {
+        id: '1',
+        description: 'Test game',
+        title: 'Test game',
+        duration: 10,
+        lastModification: '2018-11-13T20:20:39+00:00',
+        questions: [
+            {
+                id: '1',
+                type: 'QCM',
+                text: 'Combien de motifs blancs et noirs y a-t-il respectivement sur un ballon de soccer?',
+                points: 20,
+                lastModification: '2018-11-13T20:20:39+00:00',
+            },
+            {
+                id: '2',
+                type: 'QCM',
+                text: "Le ratio d'or est de 1:1.618, mais connaissez-vous le ratio d'argent?",
+                points: 40,
+                lastModification: '2024-01-20T14:17:39+00:00',
+            },
+        ],
+    };
+
+    const mockNewQuestion: Question = { id: '1', text: 'Test Question 1', type: 'QCM', points: 10, lastModification: '' };
+
+    beforeEach(() => {
+        const route = {
+            params: of({ id: '1' }),
+        };
+
+        matDialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
+
+        gamesServiceSpy = jasmine.createSpyObj('GamesService', [
+            'getGames',
+            'getGameById',
+            'toggleGameVisibility',
+            'deleteGame',
+            'uploadGame',
+            'downloadGameAsJson',
+            'replaceGame',
+        ]);
+        //matDialogSpy = TestBed.inject(MatDialog) as jasmine.SpyObj<MatDialog>;
+        questionServiceSpy = jasmine.createSpyObj('QuestionService', ['getAllQuestions', 'createQuestion']);
+        notificationServiceSpy = jasmine.createSpyObj('NotificationService', ['displayErrorMessage', 'displaySuccessMessage']);
+        questionServiceSpy.createQuestion.and.returnValue(of(mockHttpResponse));
+
+        TestBed.configureTestingModule({
+            imports: [HttpClientModule, MatDialogModule],
+            declarations: [AdminQuestionsListComponent, SortByLastModificationPipe],
+            providers: [
+                { provide: GamesService, useValue: gamesServiceSpy },
+                { provide: ActivatedRoute, useValue: route },
+                { provide: MatDialog, useValue: matDialogSpy },
+                { provide: MatSnackBar, useValue: {} },
+                { provide: NotificationService, useValue: notificationServiceSpy },
+                { provide: QuestionService, useValue: questionServiceSpy },
+            ],
+        }).compileComponents();
+
+        matDialogSpy = TestBed.inject(MatDialog) as jasmine.SpyObj<MatDialog>;
+        fixture = TestBed.createComponent(AdminQuestionsListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        component.game = mockGame;
+    });
+
     it('should create', () => {
         expect(true).toBeTruthy();
         // expect(component).toBeTruthy();
@@ -77,32 +101,65 @@ describe('AdminQuestionsListComponent', () => {
     //     expect(component.game.title).toEqual('Test');
     //     expect(component.game.description).toEqual('Test');
     // });
-    // it('should be able to change duration', () => {
-    //     const event: any = { target: { value: '20' } };
-    //     component.changeDuration(event);
-    //     expect(component.game.duration).toEqual(20);
-    // });
-    // it('should be able to delete a question from the list', () => {
-    //     const questionToDeleteId = component.game.questions[0]?.id;
-    //     if (questionToDeleteId) {
-    //         component.deleteQuestion(questionToDeleteId);
-    //     }
-    //     expect(component.game.questions.length).toBe(mockGame.questions.length);
-    // });
-    // it('should not be able to delete a question if there is only one question', () => {
-    //     component.game.questions = [mockGame.questions[0]];
-    //     const questionToDeleteId = component.game.questions[0]?.id;
-    //     if (questionToDeleteId) {
-    //         component.deleteQuestion(questionToDeleteId);
-    //     }
-    //     expect(component.game.questions.length).toBe(1);
-    // });
-    // it('should not be able to delete a question if the id is null', () => {
-    //     component.game.questions[0].id = '';
-    //     const questionToDeleteId = component.game.questions[0]?.id;
-    //     component.deleteQuestion(questionToDeleteId);
-    //     expect(component.game.questions.length).toBe(mockGame.questions.length);
-    // });
+
+    it('should be able to change duration', () => {
+        const event: any = { target: { value: '20' } };
+        component.changeDuration(event);
+        expect(component.game.duration).toEqual(20);
+    });
+
+    it('should be able to delete a question from the list', () => {
+        const questionToDeleteId = component.game.questions[0]?.id;
+        if (questionToDeleteId) {
+            component.deleteQuestion(questionToDeleteId);
+        }
+        expect(component.game.questions.length).toBe(mockGame.questions.length);
+    });
+
+    it('should not be able to delete a question if there is only one question', () => {
+        component.game.questions = [mockGame.questions[0]];
+        const questionToDeleteId = component.game.questions[0]?.id;
+        if (questionToDeleteId) {
+            component.deleteQuestion(questionToDeleteId);
+        }
+        expect(component.game.questions.length).toBe(1);
+    });
+
+    it('should not be able to delete a question if the id is null', () => {
+        component.game.questions[0].id = '';
+        const questionToDeleteId = component.game.questions[0]?.id;
+        component.deleteQuestion(questionToDeleteId);
+        expect(component.game.questions.length).toBe(mockGame.questions.length);
+    });
+
+    it('should add a new question to the current game', () => {
+        component.addNewQuestion(mockNewQuestion);
+        expect(component.game.questions).toContain(mockNewQuestion);
+        expect(component.game.questions.length).toBe(mockGame.questions.length);
+    });
+
+    it('should not add an existing question to the game', () => {
+        component.addNewQuestion(mockNewQuestion);
+        expect(notificationServiceSpy.displayErrorMessage).toHaveBeenCalled();
+    });
+
+    it('should toggle create question dialog', () => {
+        expect(component.dialogState).toBeFalse();
+        component.toggleCreateQuestion();
+        expect(component.dialogState).toBeTrue();
+    });
+
+    it('should add a question to the bank o questions', () => {
+        component.addQuestionToBank(mockNewQuestion);
+        expect(component.originalBankQuestions).toContain(mockNewQuestion);
+        expect(notificationServiceSpy.displaySuccessMessage).toHaveBeenCalled();
+    });
+
+    it('should not add question to bank if duplicate', () => {
+        component.addQuestionToBank(mockNewQuestion);
+        expect(notificationServiceSpy.displayErrorMessage).toHaveBeenCalled();
+    });
+
     // it('should be able to save the game', () => {
     //     component.saveGame();
     //     expect(gamesServiceSpy.replaceGame).toHaveBeenCalledWith(component.game);

1f63a5dddf1e029ad6ac93807c0f1e029ac01b47
Ikramarroud <112962249+Ikramarroud@users.noreply.github.com>
2024-02-10 19:32:56 -0500

    fixed ESLINT and added unit tests

--- client/src/app/pages/create-question/create-question.component.spec.ts
+++ client/src/app/pages/create-question/create-question.component.spec.ts
@@ -2,10 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { CreateQuestionComponent } from './create-question.component';
-import { GamesService } from '@app/services/games.service';
+// import { GamesService } from '@app/services/games.service';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+
+// import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+// import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
+
+// import SpyObj = jasmine.SpyObj;
 
 // wouldn't pass without this not sure why, i'm not using any animations.
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,26 +20,45 @@ describe('CreateQuestionComponent', () => {
     let component: CreateQuestionComponent;
     let fixture: ComponentFixture<CreateQuestionComponent>;
     let snackBarSpy: jasmine.SpyObj<MatSnackBar>;
+    // let gamesServiceSpy: SpyObj<GamesService>;
 
     beforeEach(() => {
         const snackBarSpyObj = jasmine.createSpyObj('MatSnackBar', ['open']);
+        // gamesServiceSpy = jasmine.createSpyObj('GamesService', [
+        //     'getGames',
+        //     'getGameById',
+        //     'toggleGameVisibility',
+        //     'deleteGame',
+        //     'uploadGame',
+        //     'downloadGameAsJson',
+        // ]);
+
         TestBed.configureTestingModule({
             declarations: [CreateQuestionComponent],
-            imports: [ReactiveFormsModule, FormsModule, MatSnackBarModule, MatSelectModule, MatFormFieldModule, MatInputModule, NoopAnimationsModule],
+            imports: [
+                // DragDropModule,
+                ReactiveFormsModule,
+                FormsModule,
+                MatSnackBarModule,
+                MatSelectModule,
+                MatFormFieldModule,
+                MatInputModule,
+                NoopAnimationsModule,
+            ],
             providers: [
-                {
-                    provide: GamesService,
-                    useValue: jasmine.createSpyObj('GamesService', [
-                        'getGames',
-                        'getGameById',
-                        'toggleGameVisibility',
-                        'deleteGame',
-                        'uploadGame',
-                        'downloadGameAsJson',
-                        'replaceGame',
-                        'verifyGame',
-                    ]),
-                },
+                // {
+                // provide: GamesService,
+                //     useValue: jasmine.createSpyObj('GamesService', [
+                //         'getGames',
+                //         'getGameById',
+                //         'toggleGameVisibility',
+                //         'deleteGame',
+                //         'uploadGame',
+                //         'downloadGameAsJson',
+                //         'replaceGame',
+                //         'verifyGame',
+                //     ]),
+                // },
                 { provide: MatSnackBar, useValue: snackBarSpyObj },
             ],
         });
@@ -43,6 +68,16 @@ describe('CreateQuestionComponent', () => {
         component = fixture.componentInstance;
 
         fixture.detectChanges();
+
+        component.questionForm.setValue({
+            text: 'Test',
+            points: 10,
+            type: 'QCM',
+            choices: [
+                { text: 'Choice 1', isCorrect: true },
+                { text: 'Choice 2', isCorrect: false },
+            ],
+        });
     });
 
     it('should create', () => {
@@ -59,48 +94,45 @@ describe('CreateQuestionComponent', () => {
 
     it('should submit the form to the list of questions in a game', () => {
         spyOn(component.createQuestionEvent, 'emit');
-        component.questionForm.setValue({
-            text: 'Test',
-            points: 10,
-            type: 'QCM',
-            choices: [
-                { text: 'Choice 1', isCorrect: true },
-                { text: 'Choice 2', isCorrect: false },
-            ],
-        });
 
         component.onSubmit();
         expect(component.createQuestionEvent.emit).toHaveBeenCalled();
     });
 
-    it('should submit the form to the bank of questions', () => {
-        spyOn(component.createQuestionEventQuestionBank, 'emit');
-        component.questionForm.setValue({
-            text: 'Test ',
-            points: 10,
-            type: 'QCM',
-            choices: [
-                { text: 'Choice 1', isCorrect: true },
-                { text: 'Choice 2', isCorrect: false },
-            ],
-        });
-        component.onSubmitQuestionBank();
-        expect(component.createQuestionEventQuestionBank.emit).toHaveBeenCalled();
+    it('should change the order of choices by drag andd dropping', () => {
+        // const mockFormArray = new FormArray([]);
+        // const spyOnDrag = spyOn(component.choices, )
+        // component.questionForm.choices = mockFormArray;
+        // component.questionForm = component.fb.group({
+        //     text: ['Sample Question Text'],
+        //     points: [10],
+        //     type: ['QCM'],
+        //     choices: mockFormArray
+        //   });
+        // omponent.addChoice();
+        // component.addChoice();
+        // const event: CdkDragDrop<any> = {
+        //   previousIndex: 0,
+        //   currentIndex: 1,
+        //   item: null!,
+        //   container: null!,
+        //   previousContainer: null!,
+        //   isPointerOverContainer: false,
+        //   distance: {x: 0, y: 0},
+        //   dropPoint: {x: 0, y: 0},
+        // };
+        // Perform the drop operation
+        // component.drop(event);
+        // Assert that the item was moved within the array
+        // expect(mockFormArray.controls.length).toEqual(0);
+        // component.updateChoiceNumbers();
+        // expect(component.createQuestionEvent.emit).toHaveBeenCalled();
     });
 
-    it('should not submit a question without at least one correct and incorrect choices', () => {
-        spyOn(component.createQuestionEvent, 'emit');
-        component.questionForm.setValue({
-            text: 'Test ',
-            points: 10,
-            type: 'QCM',
-            choices: [
-                { text: 'Choice 1', isCorrect: false },
-                { text: 'Choice 2', isCorrect: false },
-            ],
-        });
-        component.onSubmit();
-        expect(component.createQuestionEvent.emit).not.toHaveBeenCalled();
+    it('should submit the form to the bank of questions', () => {
+        spyOn(component.createQuestionEventQuestionBank, 'emit');
+        component.onSubmitQuestionBank();
+        expect(component.createQuestionEventQuestionBank.emit).toHaveBeenCalled();
     });
 
     it('should not submit a question without at least one correct and incorrect choices - Case when all is false', () => {
@@ -113,7 +145,6 @@ describe('CreateQuestionComponent', () => {
                 { text: 'Choice 1', isCorrect: false },
                 { text: 'Choice 2', isCorrect: false },
             ],
-            //lastModification: new Date().toString(),
         });
         component.onSubmit();
         expect(component.createQuestionEvent.emit).not.toHaveBeenCalled();
@@ -122,27 +153,25 @@ describe('CreateQuestionComponent', () => {
     it('should update form values when ngOnChanges is called', () => {
         spyOn(component, 'ngOnChanges').and.callThrough();
         const mockQuestion = {
-          id: '1',
-          text: 'Test Updates',
-          points: 20,
-          type: 'QCM',
-          choices: [
-            { text: 'Choice 1', isCorrect: true },
-            { text: 'Choice 2', isCorrect: false }
-          ],
-          lastModification: new Date().toLocaleString(),
+            id: '1',
+            text: 'Test Updates',
+            points: 20,
+            type: 'QCM',
+            choices: [
+                { text: 'Choice 1', isCorrect: true },
+                { text: 'Choice 2', isCorrect: false },
+            ],
+            lastModification: new Date().toLocaleString(),
         };
-        //fixture.detectChanges();
         component.question = mockQuestion;
-        component.ngOnChanges({ question: { currentValue: mockQuestion, 
-            previousValue: null, isFirstChange: () => true, 
-            firstChange: true } as any });
+        component.ngOnChanges({
+            question: { currentValue: mockQuestion, previousValue: null, isFirstChange: () => true, firstChange: true },
+        });
         component.onSubmit();
-        // Hardcoded id, onSubmit() to save the modification / add the modif date 
         component.questionForm.value.id = '1';
         expect(component.questionForm.value).toEqual(mockQuestion);
-      });
-    
+    });
+
     it('should not submit a question without at least one correct and incorrect choices - Case when all is true', () => {
         spyOn(component.createQuestionEvent, 'emit');
         component.questionForm.setValue({
@@ -165,7 +194,7 @@ describe('CreateQuestionComponent', () => {
             points: 10,
             type: 'QCM',
             choices: [
-                { text: 'Choice 1', isCorrect: true },
+                { text: 'Choice 1', isCorrect: false },
                 { text: 'Choice 2', isCorrect: true },
             ],
         });
@@ -178,7 +207,6 @@ describe('CreateQuestionComponent', () => {
         component.addChoice();
         expect(component.choices.length).toBe(initialChoicesLength + 1);
     });
-
 
     it('should remove choice from form', () => {
         component.addChoice();
@@ -197,7 +225,7 @@ describe('CreateQuestionComponent', () => {
     it('should open snack bar with the specified message : General case', () => {
         component.openSnackBar('Test message');
         expect(snackBarSpy.open).toHaveBeenCalledWith('Test message', undefined, { duration: 0 });
-      });
+    });
 
     it('should not remove more than 2 choices', () => {
         for (let i = 0; i < 3; i++) {

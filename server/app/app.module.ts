e06b979f587f56e89e6776fe81f2a583e74b3b48
Verocayden <victoca@bell.net>
2024-02-10 23:07:13 -0500

    Added AuthService and AuthController to app.module.ts

--- server/app/app.module.ts
+++ server/app/app.module.ts
@@ -2,10 +2,12 @@ import { GameController } from '@app/controllers/game/game.controller';
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthController } from './controllers/authentication/auth.controller';
 import { MatchController } from './controllers/match/match.controller';
 import { QuestionController } from './controllers/question/question.controller';
 import { Game, gameSchema } from './model/database/game';
 import { Question, questionSchema } from './model/database/question';
+import { AuthService } from './services/authentication/auth.service';
 import { GameValidationService } from './services/game-validation/game-validation.service';
 import { GameService } from './services/game/game.service';
 import { MatchService } from './services/match/match.service';
@@ -24,7 +26,7 @@ import { QuestionService } from './services/question/question.service';
         MongooseModule.forFeature([{ name: Game.name, schema: gameSchema }]),
         MongooseModule.forFeature([{ name: Question.name, schema: questionSchema }]),
     ],
-    controllers: [QuestionController, GameController, MatchController],
-    providers: [Logger, QuestionService, GameService, GameValidationService, MatchService],
+    controllers: [QuestionController, GameController, MatchController, AuthController],
+    providers: [Logger, QuestionService, GameService, GameValidationService, MatchService, AuthService],
 })
 export class AppModule {}

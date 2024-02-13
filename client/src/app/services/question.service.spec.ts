c78f9cf5366e239d99d25f16534c3f523f4da481
Abdul Chaarani <75145721+abdulchaarani@users.noreply.github.com>
2024-02-13 09:48:49 -0500

    Added tests to modifications of notification service

--- client/src/app/services/question.service.spec.ts
+++ client/src/app/services/question.service.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { QuestionService } from './question.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Question } from '@app/interfaces/question';
-// import { Observable } from 'rxjs';
 
 describe('QuestionService', () => {
     let questionService: QuestionService;

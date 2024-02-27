efa1100c7aca8bcf4f9210850fc53bb353813be7
Verocayden <victoca@bell.net>
2024-02-27 07:55:32 -0500

    Refactored Chat CSS and added Exit Button in Wait Page

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Message } from '@app/interfaces/message';
 
 @Component({
     selector: 'app-chat',
     templateUrl: './chat.component.html',
     styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent {}
+export class ChatComponent {
+    currentUsername: string = 'Utilisateur Chose Bine';
+    messages: Message[] = [
+        { author: 'Bibi', text: 'TOUPIIIIIE', date: new Date() },
+        { author: 'Bibi', text: 'WAAAH', date: new Date() },
+        { author: 'Bibi', text: 'WAAAH', date: new Date() },
+        { author: 'Bibi', text: 'WAAAH', date: new Date() },
+        { author: 'Bibi', text: 'WAAAH', date: new Date() },
+        { author: 'Bibi', text: 'WAAAH', date: new Date() },
+        { author: 'Bibi', text: 'WAAAH', date: new Date() },
+    ];
+}

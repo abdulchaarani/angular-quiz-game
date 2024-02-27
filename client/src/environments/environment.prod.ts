fb5e51102ec42f6dcd8ac38e6459dfc41a70584f
Verocayden <victoca@bell.net>
2024-02-26 19:34:42 -0500

    Updated remote server URL

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'myServerURL', // TODO: Add remote server URL
+    serverUrl: 'ec2-99-79-48-140.ca-central-1.compute.amazonaws.com', // TODO: Add remote server URL
 };

c5e1bef342b6d3e550f796b21d324abd1a3d1ba6
AdamKLahlou <kassilahlouadam@gmail.com>
2024-03-15 02:39:09 -0400

    Question area component tests selectChoice

--- client/src/app/components/question-area/question-area.component.spec.ts
+++ client/src/app/components/question-area/question-area.component.spec.ts
@@ -6,6 +6,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SocketTestHelper } from '@app/classes/socket-test-helper';
+import { Choice } from '@app/interfaces/choice';
+// import { AnswerService } from '@app/services/answer/answer.service';
 import { MatchService } from '@app/services/match/match.service';
 import { SocketHandlerService } from '@app/services/socket-handler/socket-handler.service';
 import { Socket } from 'socket.io-client';
@@ -24,6 +26,7 @@ describe('QuestionAreaComponent', () => {
   let router : spyObj<Router>;
   let socketSpy: SocketHandlerServiceMock;
   let socketHelper: SocketTestHelper;
+//   let answerServiceSpy: spyObj<AnswerService>;
 
   beforeEach(async () => {
         const mockHistoryState = {
@@ -43,6 +46,7 @@ describe('QuestionAreaComponent', () => {
             duration: 60,
         };
         router = jasmine.createSpyObj('Router', ['navigateByUrl']);
+        // answerServiceSpy = jasmine.createSpyObj('AnswerService', ['selectChoice', 'deselectChoice', 'submitAnswer', 'feedback', 'bonusPoints']);
         socketHelper = new SocketTestHelper();
         socketSpy = new SocketHandlerServiceMock(router);
         socketSpy.socket = socketHelper as unknown as Socket;
@@ -62,6 +66,7 @@ describe('QuestionAreaComponent', () => {
                 HttpClient,
                 { provide: MatchService, useValue: matchSpy },
                 { provide: SocketHandlerService, useValue: socketSpy },
+                // { provide: AnswerService, useValue: answerServiceSpy },
             ],
         }).compileComponents();
         
@@ -74,4 +79,58 @@ describe('QuestionAreaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add the choice to selectedAnswers if it is not already included', () => {
+    const choice: Choice = { text: 'London', isCorrect: false };
+    component.isSelectionEnabled = true;
+    component.selectedAnswers = [];
+
+    component.selectChoice(choice);
+
+    expect(component.selectedAnswers).toContain(choice);
+   });
+
+   it('should remove the choice from selectedAnswers if it is already included', () => {
+    const choice: Choice = { text: 'London', isCorrect: false };
+    component.isSelectionEnabled = true;
+    component.selectedAnswers = [choice];
+
+    component.selectChoice(choice);
+
+    expect(component.selectedAnswers).not.toContain(choice);
+    });
+
+    // it('should call answerService.selectChoice if context is not hostView and choice is added', () => {
+    //     const choice: Choice = { text: 'London', isCorrect: false };
+    //     component.isSelectionEnabled = true;
+    //     component.selectedAnswers = [];
+    //     component.context = 'testPage';
+
+    //     component.selectChoice(choice);
+
+    //     expect(answerServiceSpy.selectChoice).toHaveBeenCalledWith(choice.text, { username: component.username, roomCode: component.matchRoomCode });
+    // });
+
+// it('should call answerService.deselectChoice if context is not hostView and choice is removed', () => {
+//     const choice: Choice = { text: 'London', isCorrect: false };
+//     component.isSelectionEnabled = true;
+//     component.selectedAnswers = [choice];
+//     component.context = 'testPage';
+//     spyOn(component.answerService, 'deselectChoice');
+
+//     component.selectChoice(choice);
+
+//     expect(component.answerService.deselectChoice).toHaveBeenCalledWith(choice.text, { username: component.username, roomCode: component.matchRoomCode });
+// });
+
+// it('should not add or remove the choice if isSelectionEnabled is false', () => {
+//     const choice: Choice = { text: 'London', isCorrect: false };
+//     component.isSelectionEnabled = false;
+//     component.selectedAnswers = [];
+
+//     component.selectChoice(choice);
+
+//     expect(component.selectedAnswers).toEqual([]);
+// });
 });
+

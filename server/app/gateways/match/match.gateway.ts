59383c999fc7ede09b1dd6f7798866ea6c5cf8f4
AdamKLahlou <kassilahlouadam@gmail.com>
2024-03-16 20:04:02 -0400

    Asnwer service tests fully done and cleaned repeated interface declaration

--- server/app/gateways/match/match.gateway.ts
+++ server/app/gateways/match/match.gateway.ts
@@ -6,17 +6,13 @@ import { ChatService } from '@app/services/chat/chat.service';
 import { MatchBackupService } from '@app/services/match-backup/match-backup.service';
 import { MatchRoomService } from '@app/services/match-room/match-room.service';
 import { PlayerRoomService } from '@app/services/player-room/player-room.service';
+import { UserInfo } from '@common/interfaces/user-info';
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { Server, Socket } from 'socket.io';
 import { MatchEvents } from './match.gateway.events';
 
 import { ConnectedSocket, MessageBody, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-
-interface UserInfo {
-    roomCode: string;
-    username: string;
-}
 
 interface MessageInfo {
     roomCode: string;

64d14a012efc31dcda602296ddb035a38ba6ca1a
Abdul Chaarani <75145721+abdulchaarani@users.noreply.github.com>
2024-03-19 02:08:14 -0400

    question context service tests done

--- client/src/app/services/question-context/question-context.service.spec.ts
+++ client/src/app/services/question-context/question-context.service.spec.ts
@@ -13,4 +13,15 @@ describe('QuestionContextService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    it('should set context', () => {
+        service.setContext('testPage');
+        expect(service['context']).toBe('testPage');
+    });
+
+    it('should get context', () => {
+        service.setContext('testPage');
+        const context = service.getContext();
+        expect(context).toBe('testPage');
+    });
 });

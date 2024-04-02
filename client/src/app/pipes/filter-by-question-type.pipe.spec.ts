c5305a9c2656f7d8a9d67c2bf4bc7b042a2227b2
Verocayden <victoca@bell.net>
2024-04-02 17:27:14 -0400

    Revert "Added filter by question type in question bank (using pipe)"
    This reverts commit 01726571433d08c98d66fc6d5dcf76fac4ead95a.

--- client/src/app/pipes/filter-by-question-type.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterByQuestionTypePipe } from './filter-by-question-type.pipe';
-
-describe('FilterByQuestionTypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterByQuestionTypePipe();
-    expect(pipe).toBeTruthy();
-  });
-});

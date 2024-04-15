fac3f0503387526275daad89526b1634161df502
Verocayden <victoca@bell.net>
2024-04-15 16:38:12 -0400

    ES Lint Fix

--- client/src/app/components/pulse-loader/pulse-loader.component.spec.ts
+++ client/src/app/components/pulse-loader/pulse-loader.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PulseLoaderComponent } from './pulse-loader.component';
 
 describe('PulseLoaderComponent', () => {
-  let component: PulseLoaderComponent;
-  let fixture: ComponentFixture<PulseLoaderComponent>;
+    let component: PulseLoaderComponent;
+    let fixture: ComponentFixture<PulseLoaderComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [PulseLoaderComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [PulseLoaderComponent],
+        });
+        fixture = TestBed.createComponent(PulseLoaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(PulseLoaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

6e377c4f5583e9f30b272ff0bae817471c475996
Abdul Chaarani <75145721+abdulchaarani@users.noreply.github.com>
2024-04-16 00:41:41 -0400

    Fixed commented tests for 100% coverage

--- client/src/app/components/long-answer-area/long-answer-area.component.spec.ts
+++ client/src/app/components/long-answer-area/long-answer-area.component.spec.ts
@@ -1,32 +1,72 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { LongAnswerAreaComponent } from './long-answer-area.component';
-// import { SocketHandlerService } from '@app/services/socket-handler/socket-handler.service';
-
-// class SocketHandlerServiceMock extends SocketHandlerService {
-//     // Override connect() is required to not actually connect the socket
-//     // eslint-disable-next-line @typescript-eslint/no-empty-function
-//     override connect() {}
-// }
+import { LongAnswerAreaComponent } from './long-answer-area.component';
+import { MatchRoomService } from '@app/services/match-room/match-room.service';
+import { MatchContextService } from '@app/services/question-context/question-context.service';
+import { AnswerService } from '@app/services/answer/answer.service';
+import { getMockQuestion } from '@app/constants/question-mocks';
+import { Question } from '@app/interfaces/question';
+import spyObj = jasmine.SpyObj;
 
 describe('LongAnswerComponent', () => {
-    // let component: LongAnswerAreaComponent;
-    // let fixture: ComponentFixture<LongAnswerAreaComponent>;
-    // let socketSpy: SocketHandlerServiceMock;
+    let component: LongAnswerAreaComponent;
+    let fixture: ComponentFixture<LongAnswerAreaComponent>;
+    let answerSpy: spyObj<AnswerService>;
+    let matchRoomSpy: spyObj<MatchRoomService>;
+    let questionContextSpy: spyObj<MatchContextService>;
+    let mockQuestion: Question;
 
-    // beforeEach(() => {
-    //     TestBed.configureTestingModule({
-    //         declarations: [LongAnswerAreaComponent],
+    beforeEach(() => {
+        answerSpy = jasmine.createSpyObj('AnswerService', [
+            'selectChoice',
+            'deselectChoice',
+            'submitAnswer',
+            'onFeedback',
+            'onBonusPoints',
+            'onEndGame',
+            'onTimesUp',
+            'onGradeAnswers',
+            'resetStateForNewQuestion',
+            'listenToAnswerEvents',
+        ]);
 
-    //         providers: [{ provide: SocketHandlerService, useValue: socketSpy }],
-    //     });
-    //     fixture = TestBed.createComponent(LongAnswerAreaComponent);
-    //     // component = fixture.componentInstance;
-    //     fixture.detectChanges();
-    // });
+        matchRoomSpy = jasmine.createSpyObj('MatchRoomService', [
+            'goToNextQuestion',
+            'getUsername',
+            'getRoomCode',
+            'disconnect',
+            'sendPlayersData',
+            'onRouteToResultsPage',
+            'routeToResultsPage',
+            'onGameOver',
+        ]);
+
+        questionContextSpy = jasmine.createSpyObj('QuestionContextService', ['getContext']);
+
+        mockQuestion = getMockQuestion();
+        matchRoomSpy.currentQuestion = mockQuestion;
+
+        TestBed.configureTestingModule({
+            declarations: [LongAnswerAreaComponent],
+
+            providers: [
+                { provide: AnswerService, useValue: answerSpy },
+                { provide: MatchRoomService, useValue: matchRoomSpy },
+                { provide: MatchContextService, useValue: questionContextSpy },
+            ],
+        });
+        fixture = TestBed.createComponent(LongAnswerAreaComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
     it('should create', () => {
-        expect(true).toBeTruthy();
-        // expect(component).toBeTruthy();
+        expect(component).toBeTruthy();
+    });
+
+    it('should return an array of strings and AnswerCorrectness enum values', () => {
+        const answerOptions = component.answerOptions;
+        expect(answerOptions).toEqual([0, 50, 100]);
     });
 });
--- client/src/app/components/multiple-choice-area/multiple-choice-area.component.spec.ts
+++ client/src/app/components/multiple-choice-area/multiple-choice-area.component.spec.ts
@@ -1,155 +1,180 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { MultipleChoiceAreaComponent } from './multiple-choice-area.component';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { MatSnackBarModule } from '@angular/material/snack-bar';
-// import { MatDialogModule } from '@angular/material/dialog';
-// import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MultipleChoiceAreaComponent } from './multiple-choice-area.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Choice } from '@app/interfaces/choice';
+import { AnswerService } from '@app/services/answer/answer.service';
+import { MatchRoomService } from '@app/services/match-room/match-room.service';
+import { MatchContextService } from '@app/services/question-context/question-context.service';
+import spyObj = jasmine.SpyObj;
+import { getMockQuestion } from '@app/constants/question-mocks';
+import { Question } from '@app/interfaces/question';
 
 describe('MultipleChoiceAreaComponent', () => {
-    // let component: MultipleChoiceAreaComponent;
-    // let fixture: ComponentFixture<MultipleChoiceAreaComponent>;
+    let component: MultipleChoiceAreaComponent;
+    let fixture: ComponentFixture<MultipleChoiceAreaComponent>;
+    let answerSpy: spyObj<AnswerService>;
+    let matchRoomSpy: spyObj<MatchRoomService>;
+    let questionContextSpy: spyObj<MatchContextService>;
+    let mockQuestion: Question;
+    let mockChoice: Choice;
 
-    // beforeEach(() => {
-    //     TestBed.configureTestingModule({
-    //         declarations: [MultipleChoiceAreaComponent],
-    //         imports: [RouterTestingModule, HttpClientTestingModule, MatSnackBarModule, MatDialogModule, MatProgressSpinnerModule],
-    //     });
-    //     fixture = TestBed.createComponent(MultipleChoiceAreaComponent);
-    //     // component = fixture.componentInstance;
-    //     fixture.detectChanges();
-    // });
+    beforeEach(() => {
+        answerSpy = jasmine.createSpyObj('AnswerService', [
+            'selectChoice',
+            'deselectChoice',
+            'submitAnswer',
+            'onFeedback',
+            'onBonusPoints',
+            'onEndGame',
+            'onTimesUp',
+            'onGradeAnswers',
+            'resetStateForNewQuestion',
+            'listenToAnswerEvents',
+        ]);
+
+        matchRoomSpy = jasmine.createSpyObj('MatchRoomService', [
+            'goToNextQuestion',
+            'getUsername',
+            'getRoomCode',
+            'disconnect',
+            'sendPlayersData',
+            'onRouteToResultsPage',
+            'routeToResultsPage',
+            'onGameOver',
+        ]);
+
+        questionContextSpy = jasmine.createSpyObj('QuestionContextService', ['getContext']);
+
+        mockQuestion = getMockQuestion();
+        matchRoomSpy.currentQuestion = mockQuestion;
+        mockChoice = { text: 'Choice', isCorrect: false };
+        matchRoomSpy.currentQuestion.choices = [mockChoice];
+
+        TestBed.configureTestingModule({
+            declarations: [MultipleChoiceAreaComponent],
+            imports: [RouterTestingModule, HttpClientTestingModule, MatSnackBarModule, MatDialogModule, MatProgressSpinnerModule],
+            providers: [
+                { provide: AnswerService, useValue: answerSpy },
+                { provide: MatchRoomService, useValue: matchRoomSpy },
+                { provide: MatchContextService, useValue: questionContextSpy },
+            ],
+        });
+        fixture = TestBed.createComponent(MultipleChoiceAreaComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
     it('should create', () => {
         expect(true).toBeTruthy();
-        // expect(component).toBeTruthy();
+        expect(component).toBeTruthy();
     });
 
-    // it('should select a choice when a number key is pressed', () => {
-    //     const event = new KeyboardEvent('keydown', { key: '1' });
-    //     const choice: Choice = { text: 'London', isCorrect: false };
-    //     component.answers = [choice];
-    //     spyOn(component, 'selectChoice');
-    //     component.handleKeyboardEvent(event);
-    //     expect(component.selectChoice).toHaveBeenCalledWith(choice);
-    // });
+    it('should select a choice when a number key is pressed', () => {
+        const event = new KeyboardEvent('keydown', { key: '1' });
 
-    // it('should not select a choice when an invalid key is pressed', () => {
-    //     const event = new KeyboardEvent('keydown', { key: 'A' });
-    //     const choice: Choice = { text: 'London', isCorrect: false };
-    //     component.answers = [choice];
-    //     spyOn(component, 'selectChoice');
-    //     component.handleKeyboardEvent(event);
-    //     expect(component.selectChoice).not.toHaveBeenCalledWith(choice);
-    // });
+        spyOn(component, 'selectChoice');
 
-    // it('should not select a choice when there are not choices', () => {
-    //     const event = new KeyboardEvent('keydown', { key: '1' });
-    //     component.currentQuestion.choices = [];
-    //     spyOn(component, 'selectChoice');
-    //     component.handleKeyboardEvent(event);
-    //     expect(component.selectChoice).not.toHaveBeenCalled();
-    // });
+        component.handleKeyboardEvent(event);
+        expect(component.selectChoice).toHaveBeenCalledWith(mockChoice);
+    });
 
-    // it('should add the choice to selectedAnswers if it is not already included', () => {
-    //     const choice: Choice = { text: 'London', isCorrect: false };
-    //     component.isSelectionEnabled = true;
-    //     component.selectedAnswers = [];
+    it('should not select a choice when an invalid key is pressed', () => {
+        const event = new KeyboardEvent('keydown', { key: 'A' });
 
-    //     component.selectChoice(choice);
+        spyOn(component, 'selectChoice');
 
-    //     expect(component.selectedAnswers).toContain(choice);
-    // });
+        component.handleKeyboardEvent(event);
+        expect(component.selectChoice).not.toHaveBeenCalledWith(mockChoice);
+    });
 
-    // it('should remove the choice from selectedAnswers if it is already included', () => {
-    //     const choice: Choice = { text: 'London', isCorrect: false };
-    //     component.isSelectionEnabled = true;
-    //     component.selectedAnswers = [choice];
+    it('should not select a choice when there are no choices', () => {
+        const event = new KeyboardEvent('keydown', { key: '1' });
+        matchRoomSpy.currentQuestion.choices = [];
 
-    //     component.selectChoice(choice);
+        spyOn(component, 'selectChoice');
 
-    //     expect(component.selectedAnswers).not.toContain(choice);
-    // });
+        component.handleKeyboardEvent(event);
+        expect(component.selectChoice).not.toHaveBeenCalled();
+    });
 
-    // it('should not add or remove the choice if isSelectionEnabled is false', () => {
-    //     const choice: Choice = { text: 'London', isCorrect: false };
-    //     component.isSelectionEnabled = false;
-    //     component.selectedAnswers = [];
+    it('should not select a choice when chat is focused', async () => {
+        const chatInput = document.createElement('input');
+        chatInput.id = 'chat-input';
+        Object.defineProperty(document, 'activeElement', { value: chatInput, writable: true });
 
-    //     component.selectChoice(choice);
+        const event = new KeyboardEvent('keydown', { key: '1' });
+        spyOn(component, 'selectChoice');
 
-    //     expect(component.selectedAnswers).toEqual([]);
-    // });
+        component.handleKeyboardEvent(event);
+        expect(component.selectChoice).not.toHaveBeenCalled();
 
-    // it('should call answerService.selectChoice if context is not hostView and choice is added', () => {
-    //     const choice: Choice = { text: 'London', isCorrect: false };
-    //     component.isSelectionEnabled = true;
-    //     component.selectedAnswers = [];
-    //     component.context = 'testPage';
+        Object.defineProperty(document, 'activeElement', { value: component, writable: true });
+    });
 
-    //     component.selectChoice(choice);
+    it('should add the choice to selectedAnswers if it is not already included', () => {
+        answerSpy.isSelectionEnabled = true;
+        component.selectedAnswers = [];
 
-    //     expect(answerSpy.selectChoice).toHaveBeenCalledWith(choice.text, { username: component.username, roomCode: component.matchRoomCode });
-    // });
+        component.selectChoice(mockChoice);
 
-    // it('should call answerService.deselectChoice if context is not hostView and choice is removed', () => {
-    //     const choice: Choice = { text: 'London', isCorrect: false };
-    //     component.isSelectionEnabled = true;
-    //     component.selectedAnswers = [choice];
-    //     component.context = 'testPage';
+        expect(component.selectedAnswers).toContain(mockChoice);
+    });
 
-    //     component.selectChoice(choice);
+    it('should remove the choice from selectedAnswers if it is already included', () => {
+        answerSpy.isSelectionEnabled = true;
+        component.selectedAnswers = [mockChoice];
 
-    //     expect(answerSpy.deselectChoice).toHaveBeenCalledWith(choice.text, { username: component.username, roomCode: component.matchRoomCode });
-    // });
+        component.selectChoice(mockChoice);
 
-    // it('should return true if the choice is included in selectedAnswers', () => {
-    //     const choice: Choice = { text: 'London', isCorrect: false };
-    //     component.selectedAnswers = [choice];
+        expect(component.selectedAnswers).not.toContain(mockChoice);
+    });
 
-    //     expect(component.isSelected(choice)).toBeTrue();
-    // });
+    it('should not add or remove the choice if isSelectionEnabled is false', () => {
+        answerSpy.isSelectionEnabled = false;
+        component.selectedAnswers = [];
 
-    // it('should return false if the choice is not included in selectedAnswers', () => {
-    //     const choice: Choice = { text: 'London', isCorrect: false };
-    //     component.selectedAnswers = [];
+        component.selectChoice(mockChoice);
 
-    //     expect(component.isSelected(choice)).toBeFalse();
-    // });
+        expect(component.selectedAnswers).toEqual([]);
+    });
 
-    // it('should return true if the choice is included in correctAnswers', () => {
-    //     const choice: Choice = { text: 'London', isCorrect: false };
-    //     component.correctAnswers = [choice.text];
+    it('should return true if the choice is included in selectedAnswers', () => {
+        component.selectedAnswers = [mockChoice];
 
-    //     expect(component.isCorrectAnswer(choice)).toBeTrue();
-    // });
+        expect(component.isSelected(mockChoice)).toBeTrue();
+    });
 
-    // it('should return false if the choice is not included in correctAnswers', () => {
-    //     const choice: Choice = { text: 'London', isCorrect: false };
-    //     component.correctAnswers = [];
+    it('should return false if the choice is not included in selectedAnswers', () => {
+        component.selectedAnswers = [];
 
-    //     expect(component.isCorrectAnswer(choice)).toBeFalse();
-    // });
+        expect(component.isSelected(mockChoice)).toBeFalse();
+    });
 
-    // it('should reset the state for a new question when resetStateForNewQuestion is called', () => {
-    //     component.showFeedback = true;
-    //     component.isSelectionEnabled = false;
-    //     component.selectedAnswers = [{ text: 'London', isCorrect: false }];
-    //     component.bonus = 5;
-    //     component.correctAnswers = ['Paris'];
-    //     component.isRightAnswer = true;
-    //     component.isCooldown = true;
+    it('should return true if the choice is included in correctAnswers', () => {
+        answerSpy.correctAnswer = [mockChoice.text];
 
-    //     component.resetStateForNewQuestion();
+        expect(component.isCorrectAnswer(mockChoice)).toBeTrue();
+    });
 
-    //     expect(component.showFeedback).toBeFalse();
-    //     expect(component.isSelectionEnabled).toBeTrue();
-    //     expect(component.selectedAnswers).toEqual([]);
+    it('should return false if the choice is not included in correctAnswers', () => {
+        answerSpy.correctAnswer = [];
 
-    //     expect(component.bonus).toBe(0);
-    //     expect(component.correctAnswers).toEqual([]);
-    //     expect(component.isRightAnswer).toBeFalse();
-    //     expect(component.isCooldown).toBeFalse();
-    // });
+        expect(component.isCorrectAnswer(mockChoice)).toBeFalse();
+    });
+
+    it('should reset the state for a new question when resetStateForNewQuestion is called', () => {
+        answerSpy.showFeedback = true;
+        component.selectedAnswers = [mockChoice];
+
+        component['resetStateForNewQuestion']();
+
+        expect(answerSpy.showFeedback).toBeFalse();
+        expect(component.selectedAnswers).toEqual([]);
+        expect(answerSpy.resetStateForNewQuestion).toHaveBeenCalled();
+    });
 });
